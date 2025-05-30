1b50fe9bc7822c23ba5b2f9eb0be6b7bcfcad8c8
fmorini <33264532+more-ginger@users.noreply.github.com>
2025-05-30 16:23:36 +0200

    Merge pull request #14 from more-ginger/mobile-layout
    Mobile layout: add basic layout and logic for map on narrow iframes

---
+++ heat-app/src/lib/components/correlationmap/Pattern.svelte
@@ -0,0 +1,11 @@
+<defs>
+    <pattern id="dots-high" patternUnits="userSpaceOnUse" width="4" height="4">
+    <circle cx="2" cy="2" r="1.25" fill="currentColor" />
+    </pattern>
+    <pattern id="dots-medium" patternUnits="userSpaceOnUse" width="4" height="4">
+    <circle cx="2" cy="2" r="0.75" fill="currentColor" />
+    </pattern>
+    <pattern id="dots-low" patternUnits="userSpaceOnUse" width="4" height="4">
+    <circle cx="2" cy="2" r="0.25" fill="currentColor" />
+    </pattern>
+</defs>
--- heat-app/src/routes/correlationmap/+page.svelte
+++ heat-app/src/routes/correlationmap/+page.svelte
@@ -3,8 +3,9 @@
   import Map from "$lib/components/correlationmap/Map.svelte";
   import type { FeatureCollection } from "geojson";
   import { scaleSequential } from "d3-scale";
+  import {min, max} from "d3-array";
   import Legend from "$lib/components/correlationmap/Legend.svelte";
-  import { interpolateRgbBasis } from "d3-interpolate";
+  import { piecewise, interpolateRgb } from "d3-interpolate";
 
   //I needed to rewind the data here
   //https://observablehq.com/@saneef/fix-geojson
@@ -20,21 +21,49 @@
   let activeTemperatureLevel = $state("low");
 
   //SCALES
-  let heatDomain = [28, 39];
-  let heatColors = interpolateRgbBasis(["#007AF5", "#F9EFE3", "#F40000"]);
-  let heatScale = $derived(scaleSequential(heatDomain, heatColors));
+  // dynamic domain for heat scale
+  let heatMin = $derived(Math.round(min(data.features.map((d) => d.properties?.LST))));
+  let heatMax = $derived(Math.round(max(data.features.map((d) => d.properties?.LST))));
+  // Redid interpolation to add the ivory color as mid-point
+  let heatColors = piecewise(interpolateRgb, ["#005AF5", "#F9EFE3", "#FF0000"]);
+  let heatScale = $derived(scaleSequential([heatMin, heatMax], heatColors));
+
+
+  // income levels domain for legend
+  let incomeLabels =  $derived(data.features?.map((d) => d.properties?.lst_cat))
+  let incomeDomain = $derived(incomeLabels.filter((l, i) => incomeLabels.indexOf(l) === i))
 </script>
 
 <div
-  class="max-w-[1020px] max-h-[689px] m-auto flex flex-col md:flex-row p-5 border-1 border-black mt-20"
+  class="w-full max-w-[1020px] md:max-h-[689px] m-auto flex flex-col md:flex-row p-5 border-1 border-black mt-20"
 >
-  <div class="flex flex-col gap-5">
-    <h1 class="text-2xl w-60">
-      Correlation between average <span class="font-bold text-red-500">temperature</span> and
+  <div class="md:w-2/6 flex flex-col md:mr-8 md:gap-5">
+    <h1 class="text-2xl md:w-68">
+      Correlation between average 
+      <span class="font-bold text-red-500">temperature</span> and
       <span class="font-bold">poverty</span> in Berlin
     </h1>
-    <FilterMenu bind:filterActive bind:activePovertyLevel bind:activeTemperatureLevel></FilterMenu>
-    <Legend {heatDomain} {heatScale}></Legend>
+    <FilterMenu 
+      bind:filterActive 
+      bind:activePovertyLevel 
+      bind:activeTemperatureLevel
+    >
+    </FilterMenu>
+    <Legend 
+      heatDomain={[heatMin, heatMax]} 
+      {heatScale}
+      {incomeDomain}
+    >
+    </Legend>
   </div>
-  <Map {data} {activePovertyLevel} {activeTemperatureLevel} {filterActive} {heatScale}></Map>
+  <div class="md:w-4/6">
+    <Map 
+      {data} 
+      {activePovertyLevel} 
+      {activeTemperatureLevel} 
+      {filterActive} 
+      {heatScale}
+    >
+    </Map>
+  </div>
 </div>
