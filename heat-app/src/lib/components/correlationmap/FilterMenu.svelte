b80e73d7ea77ac50c380761209a9760e7c3226a3
aeschi <56318362+aeschi@users.noreply.github.com>
2025-06-15 12:43:02 +0200

    Feat: update legend text and add sources window (#32)
    * feat: add ne legend text
    
    * feat: add sources window
    
    * Feat/mobile layout (#34)
    
    * feat: make legend collapsible
    
    * feat: add mobile layout and new 660px wide layout

--- heat-app/src/lib/components/correlationmap/FilterMenu.svelte
+++ heat-app/src/lib/components/correlationmap/FilterMenu.svelte
@@ -14,22 +14,22 @@
 </script>
 
 {#snippet options()}
-  <option value="all">alle anzeigen</option>
-  <option value="low">niedrig</option>
-  <option value="medium">mittel</option>
-  <option value="high">hoch</option>
+  <option value="all">Alle anzeigen</option>
+  <option value="low">Niedrig</option>
+  <option value="medium">Mittel</option>
+  <option value="high">Hoch</option>
 {/snippet}
 
 <div class="flex flex-col text-sm gap-2.5">
   <div class="bg-white rounded-sm p-1 border-1 size-fit">
-    <label for="poverty">Armutsquote</label>
-    <select class="underline cursor-pointer" name="poverty" bind:value={activePovertyLevel}>
+    <label for="temperature cursor-pointer">Oberflächtentemperatur:</label>
+    <select class="underline" name="temperature" bind:value={activeTemperatureLevel}>
       {@render options()}
     </select>
   </div>
   <div class="bg-white rounded-sm p-1 border-1 size-fit">
-    <label for="temperature cursor-pointer">Temperatur:</label>
-    <select class="underline" name="temperature" bind:value={activeTemperatureLevel}>
+    <label for="poverty">Armutsquote</label>
+    <select class="underline cursor-pointer" name="poverty" bind:value={activePovertyLevel}>
       {@render options()}
     </select>
   </div>
